3862853441ac6e7a6e4e115ab3dfd14ceb989cce
Djones A. Boni <djboni@gmail.com>
2016-09-15 11:06:07 -0300

    Add program output

--- mnemonic.py
+++ mnemonic.py
@@ -66,3 +66,22 @@ if __name__ == "__main__":
                     mnevalue, mnemonic)
         print "    '%s' : %s," % (mnemonic, binvalue)
     print "}"
+
+# Converted mnemonics (script output)
+#
+# MNEMONICS = {
+#     'OVSPD' : '\x4D\xC6\x7D\x18',
+#     'OVREV' : '\x9F\xB3\x7D\x18',
+#     'HRACC' : '\x0C\xA3\x6C\x11',
+#     'HRBRK' : '\xD4\xB6\x6C\x11',
+#     'EXIDL' : '\x55\x23\x85\x0E',
+#     'DRVID' : '\x8D\xF4\x6D\x0D',
+#     'IGNON' : '\x17\x76\x41\x12',
+#     'FM_MD' : '\x8D\xF5\x5B\x0F',
+#     'TRP_M' : '\xD6\x9F\x6D\x1D',
+#     'TIMES' : '\x9C\x63\x49\x1D',
+#     'ODO_M' : '\xD6\x8F\x35\x18',
+#     'SPDKH' : '\x11\xD5\x64\x1C',
+#     'REVPM' : '\x56\xF6\x39\x1B',
+#     'VEHID' : '\x8D\x14\x39\x1F',
+# }
